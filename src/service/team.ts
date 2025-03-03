705e458340e05e895ab808838fe607dc16f2c21f
hayabusata <falcon.cryptomeria@gmail.com>
2025-03-03 22:26:59 +0900

    improve: 勝率分けロジック

--- src/service/team.ts
+++ src/service/team.ts
@@ -23,48 +23,21 @@ export function generateTeams(allPlayers: AllPlayers) {
     // 後衛以外
     const notAnchorPlayers = allPlayers.filter(player => !player.isAnchor)
         .sort(sortByWinCount);
-    console.log(notAnchorPlayers);
-    // for (let i = 0; i < notAnchorPlayers.length; i = i + 2) {
-    //     if ((i/2) % 2 === 0) {
-    //         alphaTeam.length < allPlayers.length/2 && alphaTeam.push(notAnchorPlayers[i]);
-    //         betaTeam.length < allPlayers.length/2 && betaTeam.push(notAnchorPlayers[i + 1]);
-    //     } else {
-    //         betaTeam.length < allPlayers.length/2 && betaTeam.push(notAnchorPlayers[i]);
-    //         alphaTeam.length < allPlayers.length/2 && alphaTeam.push(notAnchorPlayers[i + 1]);
-    //     }
-    // }
-    
 
     for (let i = 0; i < notAnchorPlayers.length / 2; i++) {
         const alphaWin = alphaTeam.reduce((acc, player) => acc + player.win, 0);
         const betaWin = betaTeam.reduce((acc, player) => acc + player.win, 0);
-        const isAlpha = alphaWin >= betaWin ? true : false;
-  
-        if (isAlpha) {
-            if (i % 2 === 0) {
-                betaTeam.length < allPlayers.length/2 && betaTeam.push(notAnchorPlayers[i]);
-                alphaTeam.length < allPlayers.length/2 && alphaTeam.push(notAnchorPlayers[i+1]);
-            } else {
-                betaTeam.length < allPlayers.length/2 && betaTeam.push(notAnchorPlayers[notAnchorPlayers.length - i]);
-                alphaTeam.length < allPlayers.length/2 && alphaTeam.push(notAnchorPlayers[notAnchorPlayers.length - i - 1]);
-            }
-        } else {
-            if (i % 2 === 0) {
-                alphaTeam.length < allPlayers.length/2 && alphaTeam.push(notAnchorPlayers[i]);
-                betaTeam.length < allPlayers.length/2 && betaTeam.push(notAnchorPlayers[i+1]);
-            } else {
-                alphaTeam.length < allPlayers.length/2 && alphaTeam.push(notAnchorPlayers[notAnchorPlayers.length - i]);
-                betaTeam.length < allPlayers.length/2 && betaTeam.push(notAnchorPlayers[notAnchorPlayers.length - i - 1]);
-            }
-        }
+        const moreAlpha = alphaWin >= betaWin ? true : false;
 
-        // if (i % 2 === 0) {
-        //     betaTeam.length < allPlayers.length/2 && betaTeam.push(notAnchorPlayers[i]);
-        //     alphaTeam.length < allPlayers.length/2 && alphaTeam.push(notAnchorPlayers[i+1]);
-        // } else {
-        //     betaTeam.length < allPlayers.length/2 && betaTeam.push(notAnchorPlayers[notAnchorPlayers.length - i - 1]);
-        //     alphaTeam.length < allPlayers.length/2 && alphaTeam.push(notAnchorPlayers[notAnchorPlayers.length - i]);
-        // }
+        if (moreAlpha) {
+            betaTeam.length < allPlayers.length/2 && betaTeam.push(notAnchorPlayers[i]);
+            alphaTeam.length < allPlayers.length/2 && alphaTeam.push(notAnchorPlayers[notAnchorPlayers.length - i - 1]);
+        } else {
+            alphaTeam.length < allPlayers.length/2 && alphaTeam.push(notAnchorPlayers[i]);
+            betaTeam.length < allPlayers.length/2 && betaTeam.push(notAnchorPlayers[notAnchorPlayers.length - i - 1]);
+
+        }
     }
+
     return {alphaTeam, betaTeam};
 }
