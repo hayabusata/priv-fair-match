8119cc6886ee3d132dca66648dfa8f36a757676c
hayabusata <falcon.cryptomeria@gmail.com>
2025-03-26 22:33:59 +0900

    add: titleとdescription

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <h1>8人用プラべチーム分け</h1>
+      <p style={{fontSize:"14px"}}>スプラトゥーンのプライベートマッチにおいて、後衛の人数およびチーム勝率を均等にチーム分けを行います。</p>
+      <p style={{fontSize:"14px"}}>名前の横のチェックボックスにチェックすると、そのプレイヤーは後衛として扱われてチーム分けされます。</p>
+      <br/>
       <Players />
       <TeamList />
     </div>
